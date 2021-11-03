6bcf3efa9f6acbda3c953173aa8a9d197774da56
jamahadrummer <jamahadrummer@live.com>
2021-11-03 08:02:31 -0500

    clean formatting

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,34 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = function (env) {
-    if (env.target === undefined) {
-        env.target = '';
-    }
+	if (env.target === undefined) {
+		env.target = '';
+	}
 
-    if (env.source === undefined) {
-        env.source = 'src';
-    }
+	if (env.source === undefined) {
+		env.source = 'src';
+	}
 
-    return {
-        mode: 'development',
-        entry: './' + env.source + '/app.js',
-        output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname, env.target, 'dist')
-        },
-        // plugins: [
-        //     new CopyPlugin({
-        //         patterns: [
-        //             {
-        //                 from: env.source + '/assets',
-        //                 to: 'assets/'
-        //             },
-        //             {
-        //                 from: env.source + '/*.(html|css)',
-        //                 to: '[name][ext]'
-        //             }
-        //         ]
-        //     })
-        // ]
-    };
+	return {
+		mode: 'development',
+		entry: './' + env.source + '/app.js',
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, env.target, 'dist')
+		},
+		// plugins: [
+		//     new CopyPlugin({
+		//         patterns: [
+		//             {
+		//                 from: env.source + '/assets',
+		//                 to: 'assets/'
+		//             },
+		//             {
+		//                 from: env.source + '/*.(html|css)',
+		//                 to: '[name][ext]'
+		//             }
+		//         ]
+		//     })
+		// ]
+	};
 };
